dc8e22da39bcf5dfb342eaf8e0a43e0a96de1332
Iago Bozza <iagobozza@writeonlycode.io>
2024-09-19 11:07:48 -0300

    Update dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,5 @@
 let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js'
+
 const tailwind = require('tailwindcss')(tailwindConfig)
 const autoprefixer = require('autoprefixer')
 
